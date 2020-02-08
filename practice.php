b578ae48edd231b71998467ef83fb0b90f80b78e
masaki <udon.207mk@gmail.com>
2020-02-08 03:26:18 +0000

    second commit

---
+++ practice.php
@@ -0,0 +1,55 @@
+<?php
+$a = "3";
+$b = "7";
+echo $a + $b;
+
+$array_month = [
+    "1月",
+    "2月",
+    "3月",
+    "4月",
+    "5月",
+    "6月",
+    "7月",
+    "8月",
+    "9月",
+    "10月",
+    "11月",
+    "12月"
+    ];
+echo $array_month[7];
+
+$hello = "hello,";
+$name =  "John";
+$world = "'s world!"
+echo $hello $name $world;
+
+$tech_boost = "tech";
+$tech_boost = "boost";
+echo $tech_boost
+
+
+?>
+
+$2018_calendar = {
+  "January" => "1月",
+  "February" => "2月"、
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" = "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => 10月,
+  "November" => "11月",
+  "December" => "12月"
+};
+
+// 12月を表示する
+echo $2018_calendar[December];
+
+間違い箇所
+1.=>が=にしなければならない
+2."2月"、→　"2月",
+3.{ }は[ ]にしなければならない。
